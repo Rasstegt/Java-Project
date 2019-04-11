da17a2745c93569d676088be6248ede89314ef55
Arslan <khalikov@sheridancollege.ca>
2019-04-10 20:17:39 -0400

    Final

--- src/beermanager/FXMLAddController.java
+++ src/beermanager/FXMLAddController.java
@@ -3,9 +3,8 @@ package beermanager;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
+import java.util.Optional;
 import java.util.ResourceBundle;
-import java.util.regex.Pattern;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,6 +15,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -40,11 +40,6 @@ public class FXMLAddController implements Initializable {
     private TextField tfPrice;
     @FXML
     private Label lblTitle;
-    
-    Parent mainInterface;
-    
-    BeerDatabase beerData = new BeerDatabase();
-    ObservableList beerList;
     @FXML
     private Button btnConfirm;
     @FXML
@@ -58,11 +53,13 @@ public class FXMLAddController implements Initializable {
     @FXML
     private Label imageTtl;
     
+    Parent mainInterface;
+    BeerDatabase beerData = new BeerDatabase();
+    ObservableList beerList;
     private String imageFile;
-    Pattern isInteger = Pattern.compile("\\d+");
     int rating, year;
     double alcoRate, price;
-    ArrayList<String> errors = new ArrayList<>();
+    Alert alert = new Alert(Alert.AlertType.WARNING);
 
 
     @Override
@@ -71,72 +68,54 @@ public class FXMLAddController implements Initializable {
     }    
     
     public void addBeer(){
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-          try {
-              
-        try {
-           price = Double.parseDouble(tfPrice.getText());
-        }catch(NumberFormatException e) {
-            errors.add("It is a price, not a poem.. "
-                + "(Double value for price)");
-            
-        }
-        try {
-           alcoRate = Double.parseDouble(tfArate.getText());
-        }catch(NumberFormatException e) {
-            errors.add("\nHave you seen an alcohol % written in String??? "
-                + "(Double for Alcohol Rate)");
-        }
         
-        try {
-           year = Integer.parseInt(tfYear.getText());
-        }catch(NumberFormatException e) {
-            errors.add("\nIf you were born in 2019.2 - "
-                + "We are so sorry for you.. (Integer for Year)");
-            
-        }
+        price = Double.parseDouble(tfPrice.getText());
+        alcoRate = Double.parseDouble(tfArate.getText());
+        year = Integer.parseInt(tfYear.getText());
+        rating = Integer.parseInt(tfRating.getText());
         
-        try {
-        if(Integer.parseInt(tfRating.getText()) > 5){
-            rating = 5;
-        } else if(Integer.parseInt(tfRating.getText()) < 0){
-            rating = 0;
-        } else {
-            rating = Integer.parseInt(tfRating.getText());
-        } 
+        if(rating > 5)  rating = 5;
+        else if(rating < 0) rating = 0;
         
-        } catch(NumberFormatException e) {
-            errors.add("\nRating.. Only 0-5, sorry (Integer for Rating)");
-        }
+        if(year > 2019) year = 2019;
+        else if(year < 0) year = 0;
         
-                beerData.createBeer(new Beer(tfName.getText(),
-                Double.parseDouble(tfArate.getText()),
-                Double.parseDouble(tfPrice.getText()),
-                rating,
-                Integer.parseInt(tfYear.getText()),
-                tfMan.getText(),
-                tfCountry.getText(), 
-                imageFile
-                ));
-                
-        } catch(NumberFormatException e){
-            
-            alert.setContentText(errors.toString() + "\nFix your errors and comeback");
-            alert.showAndWait();
-        }
+        if(alcoRate < 0) alcoRate = 0;
+        else if(alcoRate > 99) alcoRate = 99;
         
-    }
+        if(price < 0) price = 0;
 
+        beerData.createBeer(new Beer(tfName.getText(),
+        Double.parseDouble(tfArate.getText()),
+        Double.parseDouble(tfPrice.getText()),
+        rating,
+        Integer.parseInt(tfYear.getText()),
+        tfMan.getText(),
+        tfCountry.getText(), 
+        imageFile));
+           
+    }
+    
     @FXML
     private void btnConfirmHandler(ActionEvent event) {
+        try {    
         addBeer();
-        try{
-            mainInterface = FXMLLoader.load(getClass().getResource("MainInterface.fxml"));
-        }catch(IOException e){
-            System.out.println("Error: " + e);
-        }
+        } catch(NumberFormatException e) {
+        alert.setTitle("Add Record");
+        alert.setHeaderText("Be careful with values my drunk friend :)");
+        alert.setContentText("Don't fill out database if you are drunk"
+            + " (It's okay)");
+            Optional<ButtonType>result=alert.showAndWait();
+            if(result.get() == null){
+                try{
+                mainInterface = FXMLLoader.load(getClass().getResource("MainInterface.fxml"));
+                } catch(IOException ex) {
+                    System.out.println("Error: " + e);
+                }    
         Scene scene = ((Node)event.getSource()).getScene();
         scene.setRoot(mainInterface);
+            }
+        }
     }
 
     @FXML
--- src/beermanager/FXMLEditController.java
+++ src/beermanager/FXMLEditController.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -12,7 +13,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -43,19 +46,17 @@ public class FXMLEditController implements Initializable {
     private Button lbtnCancel;
     @FXML
     private Label lblError;
-    private String imageFile;
-
-    private MainInterfaceController main; 
-    
-    Beer beer;
-    
-    BeerDatabase beerData = new BeerDatabase();
     @FXML
     private ImageView iv;
     @FXML
     private Button btnImage;
     @FXML
     private Label imageTtl;
+    
+    private String imageFile;
+    private MainInterfaceController main; 
+    Beer beer;
+    BeerDatabase beerData = new BeerDatabase();
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -64,19 +65,49 @@ public class FXMLEditController implements Initializable {
 
     @FXML
     private void btnConfirmHandler(ActionEvent event) throws IOException {
+        Alert alertConfEdit = new Alert(Alert.AlertType.CONFIRMATION);
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alertConfEdit.setTitle("Edit Record");
+        alertConfEdit.setHeaderText("You are trying to edit a record");
+        alertConfEdit.setContentText("Are you sure you want to edit?");
+        Optional<ButtonType>result=alertConfEdit.showAndWait();
+        if (result.get() == ButtonType.OK){
+            try {    
         beerData.editFile(beer, 
-            tfName.getText(), 
-            Double.parseDouble(tfArate.getText()), 
-            Double.parseDouble(tfPrice.getText()), 
-            Integer.parseInt(tfRating.getText()), 
-            Integer.parseInt(tfYear.getText()) , 
-            tfMan.getText(), 
-            tfCountry.getText(), imageFile);
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("MainInterface.fxml"));
-        Parent root = (Parent)loader.load();
-        Scene scene = ((Node)event.getSource()).getScene();
-        scene.setRoot(root);
+                tfName.getText(), 
+                Double.parseDouble(tfArate.getText()), 
+                Double.parseDouble(tfPrice.getText()), 
+                Integer.parseInt(tfRating.getText()), 
+                Integer.parseInt(tfYear.getText()) , 
+                tfMan.getText(), 
+                tfCountry.getText(), 
+                imageFile);
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("MainInterface.fxml"));
+            Parent root = (Parent)loader.load();
+            
+            Scene scene = ((Node)event.getSource()).getScene();
+            scene.setRoot(root);
+            
+            } catch(Exception e) {
+                alert.setTitle("Edit Record");
+                alert.setHeaderText("Be careful with values my drunk friend :)");
+                alert.setContentText("Don't fill out database if you are drunk"
+            + " (Your mom may find out)");
+            Optional<ButtonType>answer=alert.showAndWait();
+            if(answer.get() == null){
+                try{
+                Parent root = FXMLLoader.load(getClass().getResource("MainInterface.fxml"));
+                    Scene scene = ((Node)event.getSource()).getScene();
+                    scene.setRoot(root);
+                } catch(IOException ex) {
+                    System.out.println("Error: " + e);
+                } 
+            
+        }
+    }
+            
+        }
     }
 
     @FXML
@@ -98,7 +129,8 @@ public class FXMLEditController implements Initializable {
         tfRating.setText(String.valueOf(beer.getRating()));
         tfArate.setText(String.valueOf(beer.getAlcoRate()));
         tfYear.setText(String.valueOf(beer.getYear()));
-        iv.setImage(new Image(beer.getPath()));
+        imageFile = beer.getPath();
+        iv.setImage(new Image(imageFile));        
     }
     
     public void setMain(MainInterfaceController main){
